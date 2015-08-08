c04b223486db0a18aea5c1cfd6a26090fb989db5
Levi Pihema-Lindsay <admin@2pro.co.nz>
2015-08-08 13:34:18 +1200

    Updated and created new files

--- Installer/aw.sh
+++ Installer/aw.sh
@@ -9,11 +9,11 @@ cd GitPython-0.3.2
 sudo python setup.py install
 cd
 sudo rm -rf GitPython*
-wget https://github.com/amahi/irc-bot/raw/master/Files/Releases/MS2/AmahiWorld_MS2.tar
-tar xvf AmahiWorld_MS2.tar
-rm -rf AmahiWorld_MS2.tar
+wget https://github.com/amahi/irc-bot/raw/master/Files/Releases/MS3/AmahiWorld_MS3.tar
+tar xvf AmahiWorld_MS3.tar
+rm -rf AmahiWorld_MS3.tar
 cd AmahiWorld
-wget https://github.com/amahi/irc-bot/raw/master/Files/startbot.sh
+wget https://github.com/amahi/irc-bot/raw/master/Files/Scripts/startbot.sh
 chmod 755 startbot.sh
 cd
 rm -rf amahiworld
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 irc-bot
 =======
 
-The Amahi bot, named AmahiWorld. 
+The Amahi bot, named AmahiWorld.
+
+Installation
+============
+
+To install AmahiWorld, you must be running Ubuntu 14.04.1 LTS minimal.
+From a user prompt with sudo access, run:
+
+wget git.io/amahiworld && chmod 755 amahiworld && ./amahiworld
+
+Once this is complete, all you have to do is run startbot.sh inside AmahiWorld
