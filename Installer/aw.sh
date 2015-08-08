3a878a6a30048dd80d9a6f57a97863eba408e853
Levi Pihema-Lindsay <admin@2pro.co.nz>
2015-08-09 06:18:43 +1200

    Updated Installer

--- Installer/aw.sh
+++ Installer/aw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y python-pip git wget tar zip unzip screen
+sudo apt-get install -y python-pip git wget tar zip unzip screen curl
 sudo pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt --upgrade
 sudo pip install git+https://github.com/ProgVal/Limnoria.git@master --upgrade
 wget https://pypi.python.org/packages/source/G/GitPython/GitPython-0.3.2.tar.gz
@@ -9,9 +9,10 @@ cd GitPython-0.3.2
 sudo python setup.py install
 cd
 sudo rm -rf GitPython*
-wget https://github.com/amahi/irc-bot/raw/master/Files/Releases/MS3/AmahiWorld_MS4.tar
-tar xvf AmahiWorld_MS4.tar
-rm -rf AmahiWorld_MS4.tar
+MS=$(curl https://raw.githubusercontent.com/amahi/irc-bot/master/Installer/Refs/ref)
+wget "https://raw.githubusercontent.com/amahi/irc-bot/master/Files/Releases/MS$MS/AmahiWorld_MS$MS.tar"
+tar xvf AmahiWorld_MS$MS.tar
+rm -rf AmahiWorld_MS$MS.tar
 cd AmahiWorld
 #wget https://github.com/amahi/irc-bot/raw/master/Files/Scripts/startbot.sh
 chmod 755 startbot.sh
